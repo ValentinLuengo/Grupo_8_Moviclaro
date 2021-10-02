0647f984f24d8ad37b3454f3aa2f5e0fd4cbcd2b
leleman79 <leleman79@gmail.com>
2021-10-02 08:57:42 -0300

    Cambié el nombre Design por public

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// const publicPath = path.resolve(__dirname, './public')
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/index.html'))
 });
