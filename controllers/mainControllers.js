1f88fcd897aea4dd4bc349d3afc828e7ab2caf2d
vivianama <viviana.acevedo@gmail.com>
2021-10-31 21:37:24 -0300

    agregue ir al detalle:/id, falta solucionar el error de estilos

--- controllers/mainControllers.js
+++ controllers/mainControllers.js
@@ -18,7 +18,9 @@ const mainController = {
         const requestedId = req.params.id; 
         const product =
         products.find((product) => product.id == requestedId) || products[0];
-        res.render(path.join(__dirname, '../views/products/productDetail', {product}))   
+        let pathDetalle = path.join(__dirname, '../views/products/productDetail');
+        res.render(pathDetalle, {product})  
+       //res.render('../views/products/productDetail', {product})) 
     },
 
     agregarCarrito: (req, res) => {
@@ -39,28 +41,3 @@ const mainController = {
 
 module.exports = mainController;
 
-/* 
-
-app.get('/', (req, res) => {
-    res.render('index')
-});
- app.get('/login', (req, res) => {
-    res.render('login')
-});
-app.get('/detalle', (req, res) => {
-    res.render('productDetail')
-});
-app.get('/carrito', (req, res) => {
-    res.render('productCart')
-});
-app.get('/registro', (req, res) => {
-    res.render('register')
-});
-
-app.post('/compra', function(req, res){
-    res.redirect('./')
-})
-app.post('/agregarCarrito', function(req, res){
-    res.render(__dirname, 'productCart')
-})
-*/
