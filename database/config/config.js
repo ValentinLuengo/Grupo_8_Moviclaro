be4d7e38ba9054734876729d67075a6d0e8da569
ValentinLuengo <vaklentinluengo2001@gmail.com>
2022-01-05 20:56:35 -0300

    Agrego categorias al producto

--- database/config/config.js
+++ database/config/config.js
@@ -2,7 +2,7 @@ module.exports= {
   "development": {
     "username": "root",
     "password": "",
-    "database": "moviclaro_db",
+    "database": "moviclaro",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
