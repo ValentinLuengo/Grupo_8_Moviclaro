ab5fbca54dab1fb52a372d88a1d24124991af339
pamela-londero <pamelalondero@gmail.com>
2022-02-11 16:45:35 -0300

    modifica visual dashboard

--- dashboard/src/componets/LastProduct.js
+++ dashboard/src/componets/LastProduct.js
@@ -3,7 +3,7 @@ import React from "react";
 function LastProduct(props) {
     return (
         <React.Fragment>
-            <div className="col-lg-6 mb-4">
+            <div className="col-lg-12 mb-4">
                 <div className="card shadow mb-4">
                     <div className="card-header py-3">
                         <h5 className="m-0 font-weight-bold text-gray-800">
